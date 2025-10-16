990bc439b5a164ad32033e40d1b27364f4c7c40f
Joy2441 <jtarr8@kent.edu>
2025-10-16 09:17:43 -0400

    Add initial script.js file with HTML metadata and styles

---
+++ script.js
@@ -0,0 +1,110 @@
+"><meta name="mobile-web-app-capable" content="yes"><meta name="apple-mobile-web-app-capable" content="yes"><meta name="apple-mobile-web-app-title" content="Code"><meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no"><meta name="publicBaseUrl" content="https://assets.github.dev"><link rel="shortcut icon" href="https://github.githubassets.com/favicon.ico" id="js-favicon"><link rel="manifest" href="https://solid-space-invention-g4rr75wrqw9w39vj7.github.dev/static/manifest.json"><link rel="apple-touch-icon" href="https://assets.github.dev/static/apple-icon.png"><style>body,html{margin:0;padding:0;font-size:14px}</style><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-index.js.54a5e48923e70844b195.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-bn.js.f4ff02efe84e4b991450.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-moment.js.298f0ec08c3cc511ac83.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-axios.cjs.99755f191a3c835210b6.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-_stream_readable.js.f08e52437a572f9e5e12.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-tunnelProvider.js.071e32204a9f1c3d4b74.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-sshSession.js.2f8d74d4b6ee40f6b280.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-ast.js.b916d903f5da953260fb.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-_stream_writable.js.48949b178831e15cee59.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-jsbn.js.56f28290b0e4dd9d1d6f.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-tunnelManagementHttpClient.js.da2fde81aa681ce2af41.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-source-map-consumer.js.15dc2c2ce15cb2603186.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-main.js.3351c24b6710c412d47d.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-portForwardingService.js.c88092f86f1df27fe3f6.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-util.js.cc1a21a8c606258e963e.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-secp256k1.js.7479baed2ce1affd4cc8.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-keyExchangeService.js.c3d7e30e38c45088d391.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-browser.js.ae8c8cfc08ca62be0b32.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-tunnelRelayTunnelHost.js.3146d5b05e3a8ad97e5b.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-sshProtocol.js.4a779cac1149ebd42d18.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-authenticationService.js.5ccffcd68d6771687831.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-grpc-web-client.umd.js.eaa8e3586b6b17406ca1.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-assert.js.c4e13b33302359d66134.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap-utils.js.53ba1275055909e072eb.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap-source-map-support.js.4cdcd760abc0dda6d8af.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap-short.js.211214fe1f867ee73212.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap-comparisons.js.ca0448367ef11c1e4d20.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap-assertion_error.js.fb118aded1f762e972b4.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap-sshChannel.js.2d038ebb3b345acfccb4.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/3209.4feaec42c2ca3403a119.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/7467.5765b50e936b0f8267ec.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/bootstrap.dc9bc20ad71cce32b0f1.js.download"></script><link data-name="vs/workbench/workbench.web.main" rel="stylesheet" type="text/css" href="./script.js - MYDOGPAGE1_files/workbench.web.main.internal.css"><script type="text/javascript" src="./script.js - MYDOGPAGE1_files/nls.messages.js.download"></script><style type="text/css" media="screen"></style><style>.vsonline-serverless-workbench {
+	position: absolute;
+	left: 0;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	z-index: 3;
+}
+</style><style>.cs-icon {
+    display: inline-block;
+    position: relative;
+    width: calc(1.6 * var(--grid-unit));
+    height: calc(1.6 * var(--grid-unit));
+    /* prevent flex layout to affect the sice of the icons */
+    flex-shrink: 0;
+    flex-grow: 0;
+    fill: var(--vscode-editor-foreground, #444d56);
+}
+
+.cs-icon > svg,
+.cs-icon::after {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+}
+
+.cs-icon::after {
+    content: '';
+    z-index: 2;
+}
+
+.cs-icon svg,
+.cs-icon use {
+    fill: inherit;
+    z-index: 1;
+}
+
+.cs-icon--blue {
+    fill: var(--vscode-debugIcon-continueForeground, #0366d6);
+}
+.cs-icon--green {
+    fill: var(--vscode-debugIcon-startForeground, #28a745);
+}
+.cs-icon--red {
+    fill: var(--vscode-errorForeground, #cb2431);
+}
+.cs-icon--black {
+    fill: var(--vscode-editor-foreground, #444d56);
+}
+
+.cs-icon--white {
+    fill: var(--vscode-editor-foreground, #444d56);
+}</style><style>/**
+  "><meta name="mobile-web-app-capable" content="yes"><meta name="apple-mobile-web-app-capable" content="yes"><meta name="apple-mobile-web-app-title" content="Code"><meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no"><meta name="publicBaseUrl" content="https://assets.github.dev"><link rel="shortcut icon" href="https://github.githubassets.com/favicon.ico" id="js-favicon"><link rel="manifest" href="https://solid-space-invention-g4rr75wrqw9w39vj7.github.dev/static/manifest.json"><link rel="apple-touch-icon" href="https://assets.github.dev/static/apple-icon.png"><style>body,html{margin:0;padding:0;font-size:14px}</style><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-index.js.54a5e48923e70844b195.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-bn.js.f4ff02efe84e4b991450.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-moment.js.298f0ec08c3cc511ac83.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-axios.cjs.99755f191a3c835210b6.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-_stream_readable.js.f08e52437a572f9e5e12.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-tunnelProvider.js.071e32204a9f1c3d4b74.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-sshSession.js.2f8d74d4b6ee40f6b280.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-ast.js.b916d903f5da953260fb.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-_stream_writable.js.48949b178831e15cee59.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-jsbn.js.56f28290b0e4dd9d1d6f.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-tunnelManagementHttpClient.js.da2fde81aa681ce2af41.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-source-map-consumer.js.15dc2c2ce15cb2603186.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-main.js.3351c24b6710c412d47d.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-portForwardingService.js.c88092f86f1df27fe3f6.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-util.js.cc1a21a8c606258e963e.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-secp256k1.js.7479baed2ce1affd4cc8.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-keyExchangeService.js.c3d7e30e38c45088d391.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-browser.js.ae8c8cfc08ca62be0b32.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-tunnelRelayTunnelHost.js.3146d5b05e3a8ad97e5b.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-sshProtocol.js.4a779cac1149ebd42d18.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-authenticationService.js.5ccffcd68d6771687831.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-grpc-web-client.umd.js.eaa8e3586b6b17406ca1.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap~pfHelper-assert.js.c4e13b33302359d66134.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap-utils.js.53ba1275055909e072eb.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap-source-map-support.js.4cdcd760abc0dda6d8af.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap-short.js.211214fe1f867ee73212.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap-comparisons.js.ca0448367ef11c1e4d20.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap-assertion_error.js.fb118aded1f762e972b4.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/commons-bootstrap-sshChannel.js.2d038ebb3b345acfccb4.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/3209.4feaec42c2ca3403a119.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/7467.5765b50e936b0f8267ec.js.download"></script><script defer="defer" src="./script.js - MYDOGPAGE1_files/bootstrap.dc9bc20ad71cce32b0f1.js.download"></script><link data-name="vs/workbench/workbench.web.main" rel="stylesheet" type="text/css" href="./script.js - MYDOGPAGE1_files/workbench.web.main.internal.css"><script type="text/javascript" src="./script.js - MYDOGPAGE1_files/nls.messages.js.download"></script><style type="text/css" media="screen"></style><style>.vsonline-serverless-workbench {
+	position: absolute;
+	left: 0;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	z-index: 3;
+}
+</style><style>.cs-icon {
+    display: inline-block;
+    position: relative;
+    width: calc(1.6 * var(--grid-unit));
+    height: calc(1.6 * var(--grid-unit));
+    /* prevent flex layout to affect the sice of the icons */
+    flex-shrink: 0;
+    flex-grow: 0;
+    fill: var(--vscode-editor-foreground, #444d56);
+}
+
+.cs-icon > svg,
+.cs-icon::after {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+}
+
+.cs-icon::after {
+    content: '';
+    z-index: 2;
+}
+
+.cs-icon svg,
+.cs-icon use {
+    fill: inherit;
+    z-index: 1;
+}
+
+.cs-icon--blue {
+    fill: var(--vscode-debugIcon-continueForeground, #0366d6);
+}
+.cs-icon--green {
+    fill: var(--vscode-debugIcon-startForeground, #28a745);
+}
+.cs-icon--red {
+    fill: var(--vscode-errorForeground, #cb2431);
+}
+.cs-icon--black {
+    fill: var(--vscode-editor-foreground, #444d56);
+}
+
+.cs-icon--white {
+    fill: var(--vscode-editor-foreground, #444d56);
+}</style><style>/**
